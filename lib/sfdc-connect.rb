5ffe6d60f6b869213d1de8782000f894f847de89
Scott T Weaver <scott.t.weaver@gmail.com>
2012-12-04 15:32:04 -0600

    Initial commit.

---
+++ lib/sfdc-connect.rb
@@ -0,0 +1,7 @@
+require "sfdc-connect/version"
+
+module Sfdc
+  module Connect
+    # Your code goes here...
+  end
+end
---
+++ lib/sfdc-connect/version.rb
@@ -0,0 +1,5 @@
+module Sfdc
+  module Connect
+    VERSION = "0.0.1"
+  end
+end
